5db3e6a34e4a69df065c5e650272e4dd9ed48db0
lucasvittal2 <lucasvittal@gmail.com>
2023-12-29 07:07:23 -0300

    Database were created

--- Assets/Sql/create_and_setup_database.sql
+++ Assets/Sql/create_and_setup_database.sql
@@ -1,21 +1,28 @@
-CREATE DATABASE IF NOT EXISTS 'todo_ist_db';
-USE 'todo_ist_db';
+DO $$ 
+BEGIN
+    IF NOT EXISTS (
+        SELECT FROM pg_database WHERE datname = 'todo_ist_db'
+    ) THEN
+        CREATE DATABASE todo_ist_db;
+    END IF;
+END
+$$;
+
+\c 'todo_ist_db';
 
 
 -- Drop if exist table for reason of update schemas
 
+DROP TABLE IF EXISTS Comments;
 DROP TABLE IF EXISTS Attachments;
 DROP TABLE IF EXISTS CompletedTasks;
 DROP TABLE IF EXISTS OpenedTasks;
-DROP TABLE IF EXISTS Comments;
 DROP TABLE IF EXISTS Tasks;
 DROP TABLE IF EXISTS ProductivityStats;
 DROP TABLE IF EXISTS Items;
 DROP TABLE IF EXISTS Projects;
 
-
--- create new database tables
-CREATE IF NOT EXISTS Projects(
+CREATE TABLE Projects(
     id CHAR(10) PRIMARY KEY NOT NULL,
     name VARCHAR(255),
     color VARCHAR(100),
@@ -24,76 +31,58 @@ CREATE IF NOT EXISTS Projects(
     is_inbox_project INT,
     is_shared INT,
     is_team_inbox INT,
-    order INT,
-    parent_id CHAR(10),
+    "order" INT,
+    parent_id INT,
     url VARCHAR(255),
     view_style VARCHAR(50),
-    extraction_datetime DATETIME NOT NULL
-
+    extraction_datetime TIMESTAMP NOT NULL
 );
 
-CREATE TABLE IF EXISTS Items(
-    date DATETIME,
-    project_id CHAR(10),
-    project_id refrences Projects(id)
+CREATE TABLE Items(
+    date TIMESTAMP PRIMARY KEY NOT NULL,
+    project_id CHAR(10) NOT NULL,
+    FOREIGN KEY (project_id) REFERENCES Projects(id)
 );
 
-CREATE TABLE IF EXISTS ProductivityStats(
-    date DATETIME,
+CREATE TABLE ProductivityStats(
+    date TIMESTAMP,
     total_completed INT,
-    extraction_datetime DATETIME NOT NULL
-    date refrences Items(date)
+    extraction_datetime TIMESTAMP NOT NULL,
+    FOREIGN KEY (date) REFERENCES Items(date)
 );
 
-
-CREATE TABLE IF NOT EXISTS Tasks (
+CREATE TABLE Tasks (
     id CHAR(10) PRIMARY KEY NOT NULL,
     project_id CHAR(10),
     section_id CHAR(9),
     content VARCHAR(500),
-    extraction_datetime DATETIME NOT NULL,
-    project_id refrences Projects(id)
+    extraction_datetime TIMESTAMP NOT NULL,
+    FOREIGN KEY (project_id) REFERENCES Projects(id)
 );
 
-
-CREATE TABLE  IF NOT EXISTS CompletedTasks(
+CREATE TABLE CompletedTasks(
     id CHAR(10) PRIMARY KEY NOT NULL,
     user_id CHAR(8) NOT NULL,
-    id refrences Tasks(id)
+    FOREIGN KEY (id) REFERENCES Tasks(id)
 );
 
-
-CREATE TABLE IF NOT EXISTS OpenedTasks(
+CREATE TABLE OpenedTasks(
     id CHAR(10) PRIMARY KEY NOT NULL,
     assignee_id CHAR(8) NOT NULL,
     assigner_id CHAR(8) NOT NULL,
     comment_count INT,
-    created_at DATETIME,    
+    created_at TIMESTAMP,    
     creator_id CHAR(8) NOT NULL,
     description VARCHAR(500),
-    due DATETIME
+    due TIMESTAMP,
     labels VARCHAR(255),
-    order INT,
+    "order" INT,
     parent_id CHAR(10),
     priority INT,
     url VARCHAR(255),
-    id refrences Tasks(id)
+    FOREIGN KEY (id) REFERENCES Tasks(id)
 );
-
-
-CREATE TABLE IF NOT EXISTS comments (
-    id CHAR(10)  PRIMARY KEY NOT NULL,
-    attachment_id CHAR(10) NOT NULL,
-    content VARCHAR(500),
-    posted_at DATETIME NOT NULL,
-    task_id CHAR(10) NOT NULL,
-    extraction_datetime DATETIME NOT NULL,
-    task_id refrences Tasks(id),
-    attachment_id refrences Attachments(id)
-
-);
-
-CREATE TABLE IF NOT EXISTS Attachments(
+CREATE TABLE Attachments(
     id CHAR(10) PRIMARY KEY NOT NULL,
     title VARCHAR(255),
     resource_type VARCHAR(50),
@@ -106,5 +95,17 @@ CREATE TABLE IF NOT EXISTS Attachments(
     image_width INT,
     image_height INT,
     url VARCHAR(255),
-    extraction_datetime DTETIME NOT NULL
+    extraction_datetime TIMESTAMP NOT NULL
 );
+
+CREATE TABLE comments (
+     id CHAR(10)  PRIMARY KEY NOT NULL,
+    attachment_id CHAR(10) NOT NULL,
+    content VARCHAR(500),
+    posted_at TIMESTAMP NOT NULL,
+    task_id CHAR(10) NOT NULL,
+    extraction_datetime TIMESTAMP NOT NULL,
+    FOREIGN KEY (task_id) REFERENCES Tasks(id),
+    FOREIGN KEY (attachment_id) REFERENCES Attachments(id)
+);
+
