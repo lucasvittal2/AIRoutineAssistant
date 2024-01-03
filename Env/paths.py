c0c45b78148e2caabc4219d1030f0a225462b200
lucasvittal2 <lucasvittal@gmail.com>
2024-01-03 20:12:12 -0300

    parametrization basedir on paths.py

--- Env/paths.py
+++ Env/paths.py
@@ -1,11 +1,14 @@
-from Utils.file_handler import read_yaml
+import os
 
-app_configs = read_yaml("../Assets/Configs/app_config.yaml")
-PROEJCT_PATH=app_configs['proj_base_path']
-ASSETS_PATH=PROEJCT_PATH+'Assets/'
+current_file_dir = os.path.dirname(os.path.abspath(__file__))
+project_base_dir = os.path.dirname(current_file_dir)
+
+
+PROJECT_PATH=project_base_dir + '/'
+ASSETS_PATH=PROJECT_PATH+'Assets/'
 DATA_TRACK_PATH=ASSETS_PATH+'DataTrack/'
 CONFIG_PATH=ASSETS_PATH+'Configs/'
 EXTRACTED_DATA_PATH=ASSETS_PATH+'ExtractedData/'
 ASSISTANT_FUNCTIONS_CONFIG_PATH = CONFIG_PATH+'assistant_functions_config/'
-ETL_PATH=PROEJCT_PATH+'ETL/'
+ETL_PATH=PROJECT_PATH+'ETL/'
 DRIVERS_PATH=ASSETS_PATH+'Drivers/'
